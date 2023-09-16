c75bf5c4b2fb24563cd16e3515b15ec700136872
Irfando <irfandoaritonang86@gmail.com>
2023-09-16 22:02:51 +0700

    add button delete

--- src/components/ContactItem.jsx
+++ src/components/ContactItem.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 // di kontak item akan di isi oleh ContactItemBody dan ContactItemImage
 import ContactItemBody from './ContactItemBody';
 import ContactItemImage from './ContactItemImage';
+import DeleteButton from './DeleteButton';
  
-function ContactItem({ imageUrl, name, tag }) {
+function ContactItem({ imageUrl, name, tag, id, onDelete  }) {
  return (
    <div className="contact-item">
      <ContactItemImage imageUrl={imageUrl} />
      <ContactItemBody name={name} tag={tag} />
+     <DeleteButton id={id} onDelete={onDelete} />
    </div>
  );
 }
