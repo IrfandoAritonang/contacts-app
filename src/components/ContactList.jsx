6102216c40c238b0a0ef900e7cc28a1d6d105548
Irfando <irfandoaritonang86@gmail.com>
2023-09-17 19:09:11 +0700

    edit

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import ContactItem from './ContactItem';
  
 function ContactList({ contacts, onDelete  }) {
- return (
-   <div className="contact-list">
-     {
-       contacts.map((contact) => (
-         <ContactItem 
-         key={contact.id}
-         onDelete={onDelete}
-         {...contact} />
-       ))
-     }
-   </div>
- );
+  return (
+    <div className="contact-list">
+      {
+        contacts.map((contact) => (
+          <ContactItem 
+          key={contact.id}
+          onDelete={onDelete}
+          {...contact} />
+        ))
+      }
+    </div>
+  );
 }
- 
+
 export default ContactList;  
